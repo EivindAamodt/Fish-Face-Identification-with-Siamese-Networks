7c11cd0b911e1f3cbb00988721002492c708d755
Eivind Aamodt <eivindaamodt1@gmail.com>
2020-12-02 18:06:35 +0100

    fix make directories

--- siamesenetwork/data/makedirectoriess.py
+++ siamesenetwork/data/makedirectoriess.py
@@ -1,12 +1,14 @@
 import os
 import shutil
 
-for f in os.listdir("../../../wrasse/body"):  # body or face
+test = os.getcwd()
+
+for f in os.listdir("wrasse/body"):  # body or face
     folderName = f.split("_")[0]    #treat right and left facing as same class
     # folderName = f.split("_")[0] + "_" + f.split("_")[1]  #splits between right and left facing
     if not os.path.exists(folderName):
         os.mkdir(folderName)
-        shutil.copy(os.path.join('../../../wrasse/body', f), folderName)  # body or face
+        shutil.copy(os.path.join('wrasse/body', f), folderName)  # body or face
     else:
-        shutil.copy(os.path.join('../../../wrasse/body', f), folderName)  # body or face
+        shutil.copy(os.path.join('wrasse/body', f), folderName)  # body or face
 
